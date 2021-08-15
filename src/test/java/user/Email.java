c3ae2ccf204d8b944306591096d2d9d009adf748
wodyd202 <wodyd202@naver.com>
2021-08-15 20:57:48 +0900

    [user] email 리펙토링

--- src/test/java/user/Email.java
+++ src/test/java/user/Email.java
@@ -1,6 +1,6 @@
 package user;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import lombok.NonNull;
 
 import java.io.Serializable;
 import java.util.regex.Pattern;
@@ -16,28 +16,50 @@ public class Email implements Serializable {
         mail = email;
     }
 
+    private void validation(String email) {
+        verifyNotEmptyEmail(email);
+        int at = getAt(email);
+        if(!contains(at)){
+            throw new InvalidEmailException("@ must be contain to email");
+        }
+        idOfEmailValidation(email, at);
+        hostOfEmailValidation(email, at);
+    }
+
+    private void verifyNotEmptyEmail(String email) {
+        if(email.isEmpty()){
+            throw new InvalidEmailException("email must not be empty");
+        }
+    }
+
+    private int getAt(String email) {
+        return email.indexOf("@");
+    }
+
+    private boolean contains(int at) {
+        return at == -1;
+    }
+
     /**
      * 이메일 중 아이디 부분은 영어[대,소문자], 숫자, 특수문자[._%+-] 만을 허용하고 2자 이상 20자 이하여야함
      */
     private static Pattern EMAIL_ID = Pattern.compile("^[a-zA-Z0-9._%+-]{2,20}@");
-    private static Pattern EMAIL_HOST = Pattern.compile("[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$");
-    private void validation(String email) {
-        int at = email.indexOf("@");
-        if(at == -1){
-            throw new InvalidEmailException("@ must be contain to email");
-        }
+    private void idOfEmailValidation(String email, int at) {
         String id = email.substring(0, at + 1);
         if(!EMAIL_ID.matcher(id).matches()){
             throw new InvalidEmailException("invalid id of email");
         }
+    }
 
+    private static Pattern EMAIL_HOST = Pattern.compile("[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$");
+    private void hostOfEmailValidation(String email, int at) {
         String host = email.substring(at, email.length());
         if(!EMAIL_HOST.matcher(host).matches()){
             throw new InvalidEmailException("invalid host of email");
         }
     }
 
-    public static Email of(String email) {
+    public static Email of(@NonNull String email) {
         return new Email(email);
     }
 }
--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -1,11 +1,29 @@
 package user;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class UserTest {
+
+    @DisplayName("이메일은 null값을 허용하지 않음")
+    @Test
+    void nullEmail(){
+        assertThrows(NullPointerException.class,()->{
+            Email.of(null);
+        });
+    }
+
+    @DisplayName("이메일은 빈값을 허용하지 않음")
+    @Test
+    void emptyEmail(){
+        assertThrows(InvalidEmailException.class,()->{
+           Email.of("");
+        });
+    }
 
     @DisplayName("이메일 형식중 아이디는 영문[대,소문자], 숫자, 특수문자(._%+-)만 허용하고 2자 이상 20자 이하만 가능")
     @ParameterizedTest
@@ -14,7 +32,7 @@ public class UserTest {
                             "test @naver.com",
                             "test&&@naver.com"})
     void invalidIdOfEmail(String email){
-        Assertions.assertThrows(InvalidEmailException.class,()->{
+        assertThrows(InvalidEmailException.class,()->{
             Email.of(email);
         });
     }
@@ -26,15 +44,17 @@ public class UserTest {
                             "test@.com",
                             "test@test.test.test"})
     void invalidHostOfEmail(String email){
-        Assertions.assertThrows(InvalidEmailException.class,()->{
+        assertThrows(InvalidEmailException.class,()->{
             Email.of(email);
         });
     }
 
     @DisplayName("이메일에는 @이 하나만 있어야함")
+    @Test
     void invalidEmail(){
-        Assertions.assertThrows(InvalidEmailException.class,()->{
+        assertThrows(InvalidEmailException.class,()->{
             Email.of("test@@test.com");
         });
     }
+
 }
