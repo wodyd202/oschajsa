a99210c839b10524fbcbc5e3293ea83f77ff986f
wodyd202 <wodyd202@naver.com>
2021-08-17 21:14:10 +0900

    [user] AddressHelper 주석 추가

--- src/main/java/com/ljy/oschajsa/oschajsa/user/domain/AddressHelper.java
+++ src/main/java/com/ljy/oschajsa/oschajsa/user/domain/AddressHelper.java
@@ -1,9 +1,12 @@
 package com.ljy.oschajsa.oschajsa.user.domain;
 
-import com.ljy.oschajsa.oschajsa.user.domain.AddressInfo;
-import com.ljy.oschajsa.oschajsa.user.domain.Coordinate;
-import com.ljy.oschajsa.oschajsa.user.domain.InvalidAddressException;
-
 public interface AddressHelper {
+    /**
+     * @param coordinate 좌표값
+     * - 좌표 값을 외부 API를 통해 유효성을 체크함과 동시에 해당 좌표값의 주소값을 가져옴
+     * - 좌표가 유효하지 않다면 exception 발생
+     * @return
+     * @throws InvalidAddressException
+     */
     AddressInfo getAddressInfoFrom(Coordinate coordinate) throws InvalidAddressException;
 }
