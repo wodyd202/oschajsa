b5744e8618bc0b41cd7dcc0124183d84f373d959
wodyd202 <wodyd202@daum.net>
2021-08-31 17:14:45 +0900

    [store]업체 로고 추가

--- src/main/java/com/ljy/oschajsa/oschajsa/core/file/SimpleFileUploader.java
+++ src/main/java/com/ljy/oschajsa/oschajsa/core/file/SimpleFileUploader.java
@@ -15,16 +15,9 @@ public class SimpleFileUploader implements FileUploader{
     @Value("${file-upload.url}")
     private String fileRoot;
 
-    private final String EMPTY_MESSAGE = "file extention must not be empty";
     @Override
     public String uploadFile(MultipartFile file, String fileName) {
-        String originalFileName = file.getOriginalFilename(); // 오리지날 파일명
-        if (originalFileName.equals("")) {
-            throw new IllegalArgumentException(EMPTY_MESSAGE);
-        }
-        String extension = originalFileName.substring(originalFileName.lastIndexOf(".")); // 파일 확장자
-        String savedFileName = fileName + extension; // 저장될 파일 명
-        File targetFile = new File(fileRoot + savedFileName);
+        File targetFile = new File(fileRoot + fileName);
         try {
             InputStream fileStream = file.getInputStream();
             FileUtils.copyInputStreamToFile(fileStream, targetFile); // 파일 저장
@@ -32,6 +25,6 @@ public class SimpleFileUploader implements FileUploader{
             FileUtils.deleteQuietly(targetFile); // 저장된 파일 삭제
             e.printStackTrace();
         }
-        return savedFileName;
+        return fileName;
     }
 }
